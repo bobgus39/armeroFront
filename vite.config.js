5ea42efe2e4e3509c787b159867438ba12a8a408
bobgus39 <bobgus39@gmail.com>
2026-06-01 14:17:23 +0200

    fix: configuracion Vercel y base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/armeroFront/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
